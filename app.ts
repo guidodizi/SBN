7d151a9a1f6c66766b5453c5aafce14904701f93
Guido Dizioli <guidodizi@gmail.com>
2019-03-25 16:21:33 -0300

    case 0: working cross example

--- app.ts
+++ app.ts
@@ -1,23 +1,21 @@
 import SBN from "./complier/index";
+import express = require("express");
 
 const code = `
-Paper 0 
-Pen 100 
-Line 0 50 100 50
+Paper 100 
+Pen 0
+Line 0 0 100 100
+Line 100 0 0 100
 `;
-const s = new SBN();
+const sbn = new SBN();
 
-console.log("asd");
-console.log(s.complie(code));
-// import express = require('express');
+// Create a new express application instance
+const app: express.Application = express();
 
-// // Create a new express application instance
-// const app: express.Application = express();
+app.get("/", function(req, res) {
+  res.send(sbn.complie(code));
+});
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
+});
--- build/app.js
+++ build/app.js
@@ -4,16 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var index_1 = __importDefault(require("./complier/index"));
-var code = "\nPaper 0 \nPen 100 \nLine 0 50 100 50\n";
-var s = new index_1.default();
-console.log("asd");
-console.log(s.complie(code));
-// import express = require('express');
-// // Create a new express application instance
-// const app: express.Application = express();
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
+var express = require("express");
+var code = "\nPaper 100 \nPen 0\nLine 0 0 100 100\nLine 100 0 100 0\n";
+var sbn = new index_1.default();
+// Create a new express application instance
+var app = express();
+app.get("/", function (req, res) {
+    res.send(sbn.complie(code));
+});
+app.listen(3000, function () {
+    console.log("Example app listening on port 3000!");
+});
